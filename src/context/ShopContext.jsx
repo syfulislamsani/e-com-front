a80f21d061d24a02f8f9f625e3dda235a2852ebe
Md Nasemul Islam <83567564+nasemul1@users.noreply.github.com>
2024-11-17 16:04:51 +0600

    added context

---
+++ src/context/ShopContext.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { products } from "../assets/assets";
+
+export const ShopContext = useContext();
+
+const ShopContextProvider = (props) => {
+    
+    const currency = '$';
+    const delivery_fee = 10;
+
+    const value = {
+        products, currency, delivery_fee
+    }
+
+    return (
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;
