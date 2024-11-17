6c16b2ec24f090b5889b453cc0262c9165d84fbd
Md Nasemul Islam <83567564+nasemul1@users.noreply.github.com>
2024-11-17 15:57:05 +0600

    added hero-section

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] font-outfit">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
