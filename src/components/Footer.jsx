ac20681aaf1678172e44c2a822a9f9fa3f3e1817
Md Nasemul Islam <83567564+nasemul1@users.noreply.github.com>
2025-02-23 03:36:00 +0600

    replace hero image; update Footer and Navbar components to use SVG icons instead of images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
       <div>
-        <img src={assets.logo_main} className='mb-5 w-24' alt="" />
+        <img src={assets.logo_main} className='mb-5 w-20' alt="" />
         <p className='w-full md:w-2/3 text-gray-600'>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis quibusdam ut iste sit est qui asperiores eos tempore eaque adipisci.
         </p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,10 +37,17 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className="flex items-center gap-6">
-        <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className="w-5 cursor-pointer" alt="search icon" />
+        {/* <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className="w-5 cursor-pointer" alt="search icon" /> */}
+        <svg onClick={()=>setShowSearch(true)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+        </svg>
 
         <div className="group relative">
-            <img onClick={()=>token ? null : navigate('/login')} className="w-5 cursor-pointer " src={assets.profile_icon} alt="profile icon" />
+            {/* <img onClick={()=>token ? null : navigate('/login')} className="w-5 cursor-pointer " src={assets.profile_icon} alt="profile icon" /> */}
+            <svg onClick={()=>token ? null : navigate('/login')} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 cursor-pointer">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+            </svg>
+
             {/* dropdown*/}
             { token && 
               <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
@@ -53,7 +60,10 @@ const Navbar = () => {
             }
         </div>
         <Link to="/cart" className="relative">
-            <img className="w-5 min-w-5" src={assets.cart_icon} alt="cart icon" />
+            {/* <img className="w-5 min-w-5" src={assets.cart_icon} alt="cart icon" /> */}
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+            </svg>
             <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">{getCartCount()}</p>
         </Link>
         <img onClick={()=> setVisible(true)} className="w-5 cursor-pointer sm:hidden" src={assets.menu_icon} alt="menu" />
