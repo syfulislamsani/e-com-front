9c4a5c6f90a9a13237ca1aca3a9db14fd00d2778
Md Nasemul Islam <83567564+nasemul1@users.noreply.github.com>
2025-02-23 03:03:39 +0600

    replace hero image with JPG format and update favicon path; adjust Navbar logo size and refactor Hero component structure

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,24 +3,27 @@ import { assets } from '../assets/assets'
 
 const Hero = () => {
   return (
-    <div className='flex flex-col sm:flex-row border border-gray-400'>
-        {/* Hero Left Side */}
-        <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0 font-prata'>
-            <div className='text-[#414141]'>
-                <div className='flex items-center gap-2'>
-                    <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
-                    <p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
-                </div>
-                <h1 className='text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arrivals</h1>
-                <div className='flex items-center gap-2'>
-                    <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
-                    <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
+    <>
+        <div className='flex flex-col sm:flex-row border border-gray-400'>
+            {/* Hero Left Side */}
+            <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0 font-prata'>
+                <div className='text-[#414141]'>
+                    <div className='flex items-center gap-2'>
+                        <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
+                        <p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
+                    </div>
+                    <h1 className='text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arrivals</h1>
+                    <div className='flex items-center gap-2'>
+                        <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
+                        <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
+                    </div>
                 </div>
             </div>
+            {/* Hero Right Side */}
+            <img className='w-full sm:w-1/2' src={assets.hero_img} alt="" />
         </div>
-        {/* Hero Right Side */}
-        <img className='w-full sm:w-1/2' src={assets.hero_img} alt="" />
-    </div>
+        <p className='text-[10px] italic text-right text-gray-500'>Photo by <a href="https://unsplash.com/@clemono?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Clem Onojeghuo</a> on <a href="https://unsplash.com/photos/man-in-black-coat-with-grey-scarf-standing-on-front-of-white-and-yellow-painted-wall-qGxhaKETzsY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a></p>
+    </>
   )
 }
 
